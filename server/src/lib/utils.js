aa07470f44917ac51eb32be4d628c8b6c2b82390
Ziad Mohsen <zm.ze05ad@gmail.com>
2025-05-28 00:32:28 +0300

    fix cookie configration

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: expireAtDays * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "development" ? "strict" : "none",
     secure: process.env.NODE_ENV !== "development",
   });
 
